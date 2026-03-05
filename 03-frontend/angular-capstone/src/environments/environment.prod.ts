98f16c34a0c6568061bd5f271caa3e67958eda2a
Michael Jarrett <mwilliamkjarrett@gmail.com>
2026-03-04 22:03:17 -0600

    add stripe key

--- 03-frontend/angular-capstone/src/environments/environment.prod.ts
+++ 03-frontend/angular-capstone/src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
 
   apiUrl: 'https://ecommerce-capstone-production.up.railway.app/api',
 
-  stripePublishableKey: 'pk_live_or_test_key_here',
+  stripePublishableKey: 'pk_test_51SspFKRsBlxux2f7Jht9HTU60CA8xXUVq5G9ZVjX13fGYO9vT3UpZJV7nThBDlaPrGsKFluySPWzN59FWW9AwbLJ00GZMJeOIU',
 
   auth0: {
     domain: 'dev-sdwuth2wncoe3lpg.us.auth0.com',
